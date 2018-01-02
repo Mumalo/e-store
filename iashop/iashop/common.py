8ce105be81c592bfb3a7407c7855f46adab5742b
Mumalo <ticha.muma@aun.edu.ng>
2018-01-02 18:26:59 +0100

    change media url

--- iashop/iashop/common.py
+++ iashop/iashop/common.py
@@ -231,7 +231,7 @@ STATICFILES_FINDERS = (
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 CKEDITOR_UPLOAD_PATH = "uploads/"
 
